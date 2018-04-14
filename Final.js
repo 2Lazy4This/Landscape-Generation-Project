300b7d33b20b6ac884301348b557b2d587d7e309
Nathan Coggins <25321608+2Lazy4This@users.noreply.github.com>
2018-04-13 20:19:05 -0400

    Basics Ready
    HTML Canvas we are using is there as well as basic three.js implementation. A few examples of how to use three.js are included in the Final.js file

---
+++ Final.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function main() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(50, 1.0, 0.1, 1000);
+
+    //set render to canvas Element
+    var renderer = new THREE.WebGLRenderer({canvas: drawCanvas});
+    renderer.setSize(drawCanvas.width, drawCanvas.height);
+    renderer.shadowMap.enabled = true;
+
+    var skySphereGeometry = new THREE.SphereGeometry(1000, 20, 20);  //radius, width segments, height segments
+    var skyMaterial = new THREE.MeshBasicMaterial({color: 0x8888FF, side: THREE.DoubleSide});
+    var skySphere = new THREE.Mesh(skySphereGeometry, skyMaterial);
+    skySphere.position.x = 0;
+    skySphere.position.y = 0;
+    skySphere.position.z = 0;
+    scene.add(skySphere);
+
+    var render = function () {
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    };
+
+    render();
+}
