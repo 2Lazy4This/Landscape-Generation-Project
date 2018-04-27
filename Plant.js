eb47c226e31b27c1691544fe7fd55bad1507f7ec
KateLynn Pullen <kdpullen95@gmail.com>
2018-04-27 10:05:50 -0400

    Plant Commit/Spawn Gone
    Mostly just committing to pull the new changes.

--- Plant.js
+++ Plant.js
@@ -1,13 +1,30 @@
 class Plant {
 
-  constructor(locx, locy, locz, height, branch, flex, leafshape, leafprob, color1, color2, color3) {
-
-    generate();
+  constructor(locx, locy, locz, pl) {
+    this.locx = locx;
+    this.locy = locy;
+    this.locz = locz;
+    this.objArray = [];
+    this.pl = pl;
+    rGenerate(0, 0, 0, 0, 0);
   }
 
-  generate() {
-    
+  //pl has the following attributes: height, width, branch, flex, leafshape, leafprob, skew, angle, leafstart, maxdepth, color1, color2, color3
+  rGenerate(locx, locy, locz, depth, xyAngle, zAngle) {
 
+    var height = (.75 * height) + (.25 * Math.random() * height) * 1/depth;
+    var bWidth =
+    var tWidth =
+
+    objArray.push(new plantObject(colorArray, vertArray, faceArray));
+    if (depth < pl.maxdepth) {
+      var branches = [];
+      var leaves = [];
+      depth++;
+      //TODO
+      for (i = 0; i < branches.length; i = i + 5) {
+        rGenerate(branches[i], branches[i+1], branches[i+2], branches[i+3], branches[i+4], depth);
+      }
+    }
   }
-
 }
